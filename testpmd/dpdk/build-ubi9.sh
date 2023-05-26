468126997925f51ab49452bb4562c3ca8a841023
Alberto Losada <alosadag@redhat.com>
2023-05-26 11:56:09 +0200

    Adds dpdk latest build image
    Signed-off-by: Alberto Losada <alosadag@redhat.com>

---
+++ testpmd/dpdk/build-ubi9.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DPDK_VERSION="${1:-22.11.2}"
+
+REGISTRY='quay.io/alosadag'
+PULL_SECRET="${HOME}/.docker/config.json"
+DPDK_IMAGE='dpdk-testpmd'
+LATEST_DPDK_VERSION="23.03"
+
+TAG=${DPDK_VERSION}
+if [ "${DPDK_VERSION}" == "${LATEST_DPDK_VERSION}" ]; then
+  TAG="latest"
+fi
+
+podman build -f Dockerfile-ubi9 --no-cache . \
+  --build-arg DPDK_VERSION=${DPDK_VERSION} \
+  --authfile=${PULL_SECRET} \
+  -t ${REGISTRY}/${DPDK_IMAGE}:${TAG}
+
+podman push --authfile=${PULL_SECRET} ${REGISTRY}/${DPDK_IMAGE}:${TAG}
